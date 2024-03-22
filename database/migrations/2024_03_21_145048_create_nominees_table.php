ac438122faa79cfaac0fd89cc5f1ad6b80b256ed
Robert Quainoo <softdeveloperrob@gmail.com>
2024-03-22 20:12:22 +0000

    Added unique method to userid field

--- database/migrations/2024_03_21_145048_create_nominees_table.php
+++ database/migrations/2024_03_21_145048_create_nominees_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('nominees', function (Blueprint $table) {
             $table->id();
-            $table->integer('userid');
+            $table->integer('userid')->unique();
             $table->integer('positionid');
             $table->timestamps();
         });
