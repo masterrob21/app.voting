193fc0d20a1be16477c237a83c385fd5f4fc977e
Robert Quainoo <softdeveloperrob@gmail.com>
2024-03-25 16:57:47 +0000

    Created a construct method and added the auth middleware

--- app/Http/Controllers/NomineeController.php
+++ app/Http/Controllers/NomineeController.php
@@ -93,4 +93,10 @@ class NomineeController extends Controller
     {
         //
     }
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+        // $this->middleware('');
+    }
 }
